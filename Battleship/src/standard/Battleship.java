325f92d659c3e0fffc156da5d0eef9c9f02ef339
Ferhat.Acer <Ferhat.Acer@172.16.42.155>
2024-10-16 14:18:47 +0200

    Aufgabe 2b)

--- Battleship/src/standard/Battleship.java
+++ Battleship/src/standard/Battleship.java
@@ -1,10 +1,21 @@
 package standard;
 
+
 public class Battleship {
+	
+	public static final int size = 10;
 	
 	static int distance(final Coordinate start, final Coordinate end) {
 		int sum = Math.abs(start.column() - end.column()) + Math.abs(start.row() - end.row());
 		return sum;
+	}
+	
+	static Coordinate getRandomCoordinate() {
+		int rndColumn = Utility.getRandomInt(size);
+		int rndRow = Utility.getRandomInt(size);
+		
+		return new Coordinate(rndColumn, rndRow);
+		
 	}
 
 }
